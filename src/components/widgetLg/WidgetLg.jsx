60501052e09de56f291e520337939d3f78251966
nesar11 <nesar.uddin100@gmail.com>
2022-03-21 23:29:52 +0800

    widgetSm

---
+++ src/components/widgetLg/WidgetLg.jsx
@@ -0,0 +1,10 @@
+import "./WidgetLg.css"
+
+export default function WidgetLg() {
+  return (
+    <div className="widgetLg">
+        WidgetLage
+      
+    </div>
+  )
+}
---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,76 @@
+import "./WidgetSm.css"
+import {
+  Visibility
+    
+  } from "@material-ui/icons";
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+        <span className="widgetSmTitle">New Join Members</span>
+        <ul className="widgetSmList">
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/8358677/pexels-photo-8358677.jpeg" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                <span className="widgetSmUserName"> Lee Smith</span>
+                    <span className="widgetSmUserTitle"> Chief Chef</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/8358677/pexels-photo-8358677.jpeg" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                <span className="widgetSmUserName"> Lee Smith</span>
+                    <span className="widgetSmUserTitle"> Chief Chef</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/8358677/pexels-photo-8358677.jpeg" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                <span className="widgetSmUserName"> Lee Smith</span>
+                    <span className="widgetSmUserTitle"> Chief Chef</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/8358677/pexels-photo-8358677.jpeg" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUserName"> Lee Smith</span>
+                    <span className="widgetSmUserTitle"> Chief Chef</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/8358677/pexels-photo-8358677.jpeg" alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUserName"> Lee Smith</span>
+                    <span className="widgetSmUserTitle"> Chief Chef</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+
+                </button>
+            </li>
+
+        </ul>
+
+    </div>
+  )
+}
